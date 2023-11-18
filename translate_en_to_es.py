919251dbb08e62441621a35826fd209798b843e3
bananighosh <bg2502@nyu.edu>
2023-11-18 03:24:26 -0500

    Implemented OpenNMT

--- translate_en_to_es.py
+++ translate_en_to_es.py
@@ -3,10 +3,38 @@ import glob
 import json
 import os
 
+import argostranslate.package
+import argostranslate.translate
+
 from pprint import pprint
 from translate import Translator
 # from PyDictionary import PyDictionary 
 from PyMultiDictionary import MultiDictionary
+
+def download_and_install_package(from_code, to_code):
+    # Download and install Argos Translate package
+    argostranslate.package.update_package_index()
+    available_packages = argostranslate.package.get_available_packages()
+    package_to_install = next(
+        filter(
+            lambda x: x.from_code == from_code and x.to_code == to_code, available_packages
+        )
+    )
+    argostranslate.package.install_from_path(package_to_install.download())
+
+def translate_en_to_es_nmt(src_sentence):
+    print(f"openNMT")
+
+    from_code = "en"
+    to_code = "es"
+
+    download_and_install_package(from_code, to_code)
+
+    # Translate
+    # translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
+    translatedText = argostranslate.translate.translate(src_sentence, from_code, to_code)
+    # print(translatedText)
+    return translatedText
 
 def translate_to_spanish(src, dest, target_language_code):
     # target language chosen is Spanish -> espionel - > code : "es"
@@ -40,7 +68,8 @@ def translate_to_spanish(src, dest, target_language_code):
             #     chunk = caption['text'][i:i + chunk_size]
             #     chunk_translation = translator.translate(chunk)
             #     translation += chunk_translation
-            caption['text'] = translator.translate(caption["text"])
+            # caption['text'] = translator.translate(caption["text"]) this worked
+            caption['text'] = translate_en_to_es_nmt(caption["text"])
 
         dest_file = os.path.join(dest, os.path.basename(file))
         print(f"saving result to {dest_file}")
@@ -79,5 +108,7 @@ if __name__ == '__main__':
     src_en_folder = "./transcriptions_en"
     destination_es_folder = "./transcriptions_es"
     pathlib.Path(destination_es_folder).mkdir(parents=True, exist_ok=True)
-    translate_to_spanish(src_en_folder, destination_es_folder, "es")
+    # translate_to_spanish(src_en_folder, destination_es_folder, "es")
+    translate_en_to_es_nmt()
+
     # translate_to_spanish_dict(src_en_folder, destination_es_folder, "es")
