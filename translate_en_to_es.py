e0f9a18889efb5f7bafc6760e37ce312e73e16f2
Rafik Saad <rs7983@nyu.edu>
2023-11-19 15:24:10 +0000

    minor cleaning/tweaks

--- translate_en_to_es.py
+++ translate_en_to_es.py
@@ -1,17 +1,12 @@
 import pathlib
-import glob
 import json
-import os
+import glob
 
 import argostranslate.package
 import argostranslate.translate
 
-from pprint import pprint
-from translate import Translator
-# from PyDictionary import PyDictionary 
-from PyMultiDictionary import MultiDictionary
 
-def download_and_install_package(from_code, to_code):
+def download_and_install_package(from_code="en", to_code="es"):
     # Download and install Argos Translate package
     argostranslate.package.update_package_index()
     available_packages = argostranslate.package.get_available_packages()
@@ -22,93 +17,70 @@ def download_and_install_package(from_code, to_code):
     )
     argostranslate.package.install_from_path(package_to_install.download())
 
-def translate_en_to_es_nmt(src_sentence):
-    print(f"openNMT")
 
-    from_code = "en"
-    to_code = "es"
+def translate_sentence(sentence, from_code="en", to_code="es"):
+    return argostranslate.translate.translate(sentence, from_code, to_code)
 
-    download_and_install_package(from_code, to_code)
 
-    # Translate
-    # translatedText = argostranslate.translate.translate("Hello World", from_code, to_code)
-    translatedText = argostranslate.translate.translate(src_sentence, from_code, to_code)
-    # print(translatedText)
-    return translatedText
+def translate_file(trans: dict, from_code="en", to_code="es")-> dict:
+    """transelate english transcription json file in to spanish and save output json to <save_path>"""
+    # transelate each sentence from en -> es 
+    # while preserving attributes (start time, duration etc..)
+    for segment in trans['segments']:
+        segment['text'] = translate_sentence(segment['text'], from_code, to_code)
 
-def translate_to_spanish(src, dest, target_language_code):
-    # target language chosen is Spanish -> espionel - > code : "es"
-    if (not pathlib.Path(src).exists() 
-        or not pathlib.Path(dest).exists()):
-        raise ValueError("video or destination folder do not exist")
+    # transelate text paragraph (without additional attributes)
+    trans['text'] = translate_sentence(trans['text'], from_code, to_code)
+
+    # set language to es
+    trans['language'] = to_code
+
+    return trans
+
+
+def translate_all_files(source_directory, destination_directory, from_code="en", to_code="es"):
+    """transelates all english Json transcription files in <source_directory
+        to spanish Json files and saves them to <destination_directory>
+        while keeping same file name and other attributes"""
     
-    files = glob.glob(src + "/*.json")
-    if not files:
-        raise ValueError("No JSON files found in given directory")
-    
-    for file in files:
-        # captions = {}
+    en_transcriptsion_directory = source_directory
+    es_transcriptsion_directory = destination_directory
+
+    # get a list of all json files in directory
+    en_files = glob.glob(en_transcriptsion_directory + "/*.json")
+
+    if not en_files:
+        raise ValueError(f"no json files found in {source_directory}")
+
+    # create destination driectory if it does not exist
+    pathlib.Path(es_transcriptsion_directory).mkdir(parents=True, exist_ok=True)
+
+    print(f"source_directory: {en_transcriptsion_directory}")
+    print(f"destination_driectory {es_transcriptsion_directory}")
+    print(f"Translating from {from_code} to {to_code}")
+    print("")  # new line
+    for file_path in en_files:
+        print(f"Translating: {pathlib.Path(file_path).name}...", end="")
+        save_path = str(pathlib.Path(es_transcriptsion_directory) / pathlib.Path(file_path).name)
         
-        with open(file) as f:
-            captions = json.load(f)
-        # print(caption["text"])
+        with open(file_path, 'r') as file:
+            en_trans = json.load(file)
 
-        print(f"translating {os.path.basename(file)}...", end="")
-        # text = " Not since Watergate has an attorney general been at the center of such a firestorm."
-        translator = Translator(to_lang=target_language_code)
+        es_trans = translate_file(en_trans, from_code, to_code)
 
-        # translation = translator.translate(caption["text"])
-        # caption['text'] = translation
-        # print(f"{ caption['text']} in spanish: {translation}")
-        for caption in captions["segments"]:
-            # Split the input text into chunks with a maximum length of 500 characters
-            # chunk_size = 500
-            # translation = ""
-            # for i in range(0, len(caption['text']), chunk_size):
-            #     chunk = caption['text'][i:i + chunk_size]
-            #     chunk_translation = translator.translate(chunk)
-            #     translation += chunk_translation
-            # caption['text'] = translator.translate(caption["text"]) this worked
-            caption['text'] = translate_en_to_es_nmt(caption["text"])
+        with open(save_path, 'w') as outfile:
+            outfile.write(json.dumps(en_trans))
+        
+        print("success!")
 
-        dest_file = os.path.join(dest, os.path.basename(file))
-        print(f"saving result to {dest_file}")
-        with open(dest_file, "w", encoding="utf-8") as file:
-            file.write(json.dumps(captions) + "\n")
+    return None
 
-
-# def translate_to_spanish_dict(src, dest, target_language_code):
-#     dict = MultiDictionary () 
-  
-#     # to translate into German 
-#     translation = dict.translate('en', "happy", to=target_language_code) 
-#     print(translation) 
-#     # return translation
-
-
-# def create_en_to_es_dictionary():
-#     test_keys = ["Rash", "Kil", "Varsha"]
-#     test_values = [1, 4, 5]
- 
-#     # Printing original keys-value lists
-#     print ("Original key list is : " + str(test_keys))
-#     print ("Original value list is : " + str(test_values))
-    
-    
-#     # create a list of tuples using enumerate()
-#     tuples = [(key, value)
-#             for i, (key, value) in enumerate(zip(test_keys, test_values))]
-    
-#     # convert list of tuples to dictionary using dict()
-#     res = dict(tuples)
-    
-#     print ("Resultant dictionary is : " + str(res))
 
 if __name__ == '__main__':
-    src_en_folder = "./transcriptions_en"
-    destination_es_folder = "./transcriptions_es"
-    pathlib.Path(destination_es_folder).mkdir(parents=True, exist_ok=True)
-    # translate_to_spanish(src_en_folder, destination_es_folder, "es")
-    translate_en_to_es_nmt()
-
-    # translate_to_spanish_dict(src_en_folder, destination_es_folder, "es")
+    from_code = 'en'
+    to_code = 'es'
+    source_directory = "./transcriptions_en"
+    destination_directory = "./transcriptions_es"
+    
+    download_and_install_package(from_code, to_code)
+    translate_all_files(source_directory, destination_directory)
